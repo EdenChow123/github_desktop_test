f89531edd724077c33971fc0a73a4e8122faaa15
EdenChan <h2276252@qq.com>
2026-07-05 19:51:38 +0800

    update_file
    update new file

---
+++ swiftUI_demo_testGroup.swift
@@ -0,0 +1,32 @@
+//
+//  swiftUI_demo_testGroup.swift
+//  swiftUI_demo_testGroup
+//
+//  Created by Eden on 2026/4/1.
+//
+
+import XCTest
+@testable import swiftUI_demo
+
+final class swiftUI_demo_testGroup: XCTestCase {
+
+    var vm: UnitTestViewModel?
+    
+    override func setUpWithError() throws {
+        vm = UnitTestViewModel(isVip: false)
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    
+ 
+
+    func test_unitTestViewModel() {
+        
+        let _isVip = false
+        XCTAssertEqual(false, _isVip)
+    }
+
+}
